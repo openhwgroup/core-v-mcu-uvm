17aea386315310fb776d1d9c3526acff20d41097
datum-dpoulin <dpoulin@datumtc.ca>
2022-06-23 15:47:42 -0400

    switching to using 'main' mio cli branch

--- sim/setup_project.py
+++ sim/setup_project.py
@@ -1,14 +1,7 @@
 #! /usr/bin/python3 
 ########################################################################################################################
 ## Copyright 2022 Datum Technology Corporation
-########################################################################################################################
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
-## Licensed under the Solderpad Hardware License v 2.1 (the "License"); you may not use this file except in compliance
-## with the License, or, at your option, the Apache License version 2.0.  You may obtain a copy of the License at
-##                                        https://solderpad.org/licenses/SHL-2.1/
-## Unless required by applicable law or agreed to in writing, any work distributed under the License is distributed on
-## an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
-## specific language governing permissions and limitations under the License.
 ########################################################################################################################
 
 
@@ -79,7 +72,7 @@ def copy_tree(src, dst, symlinks=False, ignore=None):
 def clone_mio_cli():
     tools_ip_name = "mio"
     dst_path = tools_imports_path + "/" + tools_ip_name
-    branch = "old_cli"
+    branch = "main"
     uri = "https://github.com/Datum-Technology-Corporation/mio_client.git"
     
     if not os.path.exists(tools_imports_path):
