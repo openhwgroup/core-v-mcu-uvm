a82aa09dcf0034e054dfdc33c1927248a12f4c5f
David Poulin <dpoulin@datumtc.ca>
2022-07-21 17:56:41 -0400

    Added UVM Register model for uvme_apb_timer

--- dv/uvme_apb_timer/src/reg/uvme_apb_timer_reg_block.sv
+++ dv/uvme_apb_timer/src/reg/uvme_apb_timer_reg_block.sv
@@ -1,147 +1,202 @@
-// Copyright 2022 Datum Technology Corporation
-// Copyright 2022 Low Power Futures
-// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Copyright 2021 Low Power Futures Inc.
+// 
+
+
+// WARNING: THE FOLLOWING IS AUTO-GENERATED CODE.
+// ANY CHANGES MADE HERE WILL BE ERASED UPON THE NEXT RELEASE OF THIS RAL.
 
 
 `ifndef __UVME_APB_TIMER_REG_BLOCK_SV__
 `define __UVME_APB_TIMER_REG_BLOCK_SV__
 
 
-/**
- * Top-level object encapsulating APB Timer Register Block.
- */
+`include "uvme_apb_timer_cfg_reg_low_reg.sv"
+`include "uvme_apb_timer_cfg_reg_hi_reg.sv"
+`include "uvme_apb_timer_timer_val_lo_reg.sv"
+`include "uvme_apb_timer_timer_val_hi_reg.sv"
+`include "uvme_apb_timer_timer_cmp_lo_reg.sv"
+`include "uvme_apb_timer_timer_cmp_hi_reg.sv"
+`include "uvme_apb_timer_timer_start_lo_reg.sv"
+`include "uvme_apb_timer_timer_start_hi_reg.sv"
+`include "uvme_apb_timer_timer_reset_lo_reg.sv"
+`include "uvme_apb_timer_timer_reset_hi_reg.sv"
+
+
 class uvme_apb_timer_reg_block_c extends uvml_ral_reg_block_c;
-
-   /// @defgroup Objects
-   /// @{
-   uvme_apb_timer_cfg_c    cfg  ; ///< Environment configuration handle
-   uvme_apb_timer_cntxt_c  cntxt; ///< Environment context handle
-   /// @}
-
-   /// @defgroup Sub-Blocks
-   /// @{
-   // TODO Add sub-block(s)
-   //      Ex: rand uvme_apb_timer_abc_reg_block_c  abc; ///< Describe me!
-   /// @}
-
-   /// @defgroup Registers
-   /// @{
-   // TODO Add register(s)
-   //      Ex: rand uvme_apb_timer_xyz_reg_c  xyz; ///< Describe me!
-   /// @}
-
-
+   
+   // Objects
+   uvme_apb_timer_cfg_c    cfg;
+   uvme_apb_timer_cntxt_c  cntxt;
+   
+   // Registers
+  rand uvme_apb_timer_cfg_reg_low_reg_c  cfg_reg_low;
+  rand uvme_apb_timer_cfg_reg_hi_reg_c  cfg_reg_hi;
+  rand uvme_apb_timer_timer_val_lo_reg_c  timer_val_lo;
+  rand uvme_apb_timer_timer_val_hi_reg_c  timer_val_hi;
+  rand uvme_apb_timer_timer_cmp_lo_reg_c  timer_cmp_lo;
+  rand uvme_apb_timer_timer_cmp_hi_reg_c  timer_cmp_hi;
+  rand uvme_apb_timer_timer_start_lo_reg_c  timer_start_lo;
+  rand uvme_apb_timer_timer_start_hi_reg_c  timer_start_hi;
+  rand uvme_apb_timer_timer_reset_lo_reg_c  timer_reset_lo;
+  rand uvme_apb_timer_timer_reset_hi_reg_c  timer_reset_hi;
+   
+   
    `uvm_object_utils_begin(uvme_apb_timer_reg_block_c)
-      // TODO Add field macros for sub-block(s) and register(s)
-      //      Ex: `uvm_field_object(abc, UVM_DEFAULT)
-      //          `uvm_field_object(xyz, UVM_DEFAULT)
+      `uvm_field_object(cfg_reg_low, UVM_DEFAULT)
+      `uvm_field_object(cfg_reg_hi, UVM_DEFAULT)
+      `uvm_field_object(timer_val_lo, UVM_DEFAULT)
+      `uvm_field_object(timer_val_hi, UVM_DEFAULT)
+      `uvm_field_object(timer_cmp_lo, UVM_DEFAULT)
+      `uvm_field_object(timer_cmp_hi, UVM_DEFAULT)
+      `uvm_field_object(timer_start_lo, UVM_DEFAULT)
+      `uvm_field_object(timer_start_hi, UVM_DEFAULT)
+      `uvm_field_object(timer_reset_lo, UVM_DEFAULT)
+      `uvm_field_object(timer_reset_hi, UVM_DEFAULT)
    `uvm_object_utils_end
-
-
+   
+   
    /**
     * Default constructor.
     */
-   extern function new(string name="uvme_apb_timer_reg_block", int has_coverage=UVM_NO_COVERAGE);
-
-   /**
-    * Creates register and register block instances and associates them with this register block.
-    */
-   extern virtual function void build();
-
-   /**
-    * Associates sub-register blocks and registers with this register block.
-    */
-   extern virtual function void connect();
-
-   /**
-    * Creates sub-block(s).
-    */
-   extern function void create_blocks();
-
+   extern function new(string name="uvme_apb_timer_reg_block");
+   
    /**
     * Creates register(s).
     */
-   extern function void create_regs();
-
+   extern virtual function void create_regs();
+   
    /**
-    * Creates register map.
+    * Creates default register map.
     */
-   extern function void create_reg_map();
-
+   extern virtual function void create_reg_map();
+   
    /**
     * Adds register(s) to register map.
     */
-   extern function void add_regs_to_map();
-
+   extern virtual function void add_regs_to_map();
+   
 endclass : uvme_apb_timer_reg_block_c
 
 
-function uvme_apb_timer_reg_block_c::new(string name="uvme_apb_timer_reg_block", int has_coverage=UVM_NO_COVERAGE);
-
-   super.new(name, has_coverage);
-
+function uvme_apb_timer_reg_block_c::new(string name="uvme_apb_timer_reg_block");
+   
+   super.new(name, UVM_NO_COVERAGE);
+   
 endfunction : new
 
 
-function void uvme_apb_timer_reg_block_c::build();
-
-   create_blocks  ();
-   create_regs    ();
-   create_reg_map ();
-
-endfunction: build
-
-
-function void uvme_apb_timer_reg_block_c::connect();
-
-   add_regs_to_map();
-   lock_model     ();
-
-endfunction: connect
-
-
-function void uvme_apb_timer_reg_block_c::create_blocks();
-
-   // TODO Implement uvme_apb_timer_reg_block_c::create_blocks()
-   //      Ex: abc = uvme_apb_timer_abc_reg_block_c::type_id::create("abc");
-   //          abc.configure(this);
-   //          abc.build();
-
-endfunction : create_blocks
-
-
 function void uvme_apb_timer_reg_block_c::create_regs();
-
-   // TODO Implement uvme_apb_timer_reg_block_c::create_regs()
-   //      Ex:  xyz = uvme_apb_timer_xyz_reg_c::type_id::create("xyz");
-   //           xyz.configure(this);
-   //           xyz.build();
-
+   
+   cfg_reg_low = uvme_apb_timer_cfg_reg_low_reg_c::type_id::create("cfg_reg_low");
+   cfg_reg_low.configure(this);
+   cfg_reg_low.build();
+   
+   cfg_reg_hi = uvme_apb_timer_cfg_reg_hi_reg_c::type_id::create("cfg_reg_hi");
+   cfg_reg_hi.configure(this);
+   cfg_reg_hi.build();
+   
+   timer_val_lo = uvme_apb_timer_timer_val_lo_reg_c::type_id::create("timer_val_lo");
+   timer_val_lo.configure(this);
+   timer_val_lo.build();
+   
+   timer_val_hi = uvme_apb_timer_timer_val_hi_reg_c::type_id::create("timer_val_hi");
+   timer_val_hi.configure(this);
+   timer_val_hi.build();
+   
+   timer_cmp_lo = uvme_apb_timer_timer_cmp_lo_reg_c::type_id::create("timer_cmp_lo");
+   timer_cmp_lo.configure(this);
+   timer_cmp_lo.build();
+   
+   timer_cmp_hi = uvme_apb_timer_timer_cmp_hi_reg_c::type_id::create("timer_cmp_hi");
+   timer_cmp_hi.configure(this);
+   timer_cmp_hi.build();
+   
+   timer_start_lo = uvme_apb_timer_timer_start_lo_reg_c::type_id::create("timer_start_lo");
+   timer_start_lo.configure(this);
+   timer_start_lo.build();
+   
+   timer_start_hi = uvme_apb_timer_timer_start_hi_reg_c::type_id::create("timer_start_hi");
+   timer_start_hi.configure(this);
+   timer_start_hi.build();
+   
+   timer_reset_lo = uvme_apb_timer_timer_reset_lo_reg_c::type_id::create("timer_reset_lo");
+   timer_reset_lo.configure(this);
+   timer_reset_lo.build();
+   
+   timer_reset_hi = uvme_apb_timer_timer_reset_hi_reg_c::type_id::create("timer_reset_hi");
+   timer_reset_hi.configure(this);
+   timer_reset_hi.build();
+   
 endfunction : create_regs
 
 
 function void uvme_apb_timer_reg_block_c::create_reg_map();
-
+   
+   // Create default register map (default_map)
    default_map = create_map(
       .name     ("default_map"),
-      .base_addr(cfg.reg_block_base_address),
-      .n_bytes  (uvme_apb_timer_reg_block_reg_n_bytes),
+      .base_addr(32'h1A10_B000),
+      .n_bytes  (4),
       .endian   (UVM_LITTLE_ENDIAN)
    );
-
+   
 endfunction : create_reg_map
 
 
 function void uvme_apb_timer_reg_block_c::add_regs_to_map();
-
-   // TODO Implement uvme_apb_timer_reg_block_c::add_regs_to_map()
-   //      Ex: default_map.add_reg(
-   //             .rg    (xyz),
-   //             .offset(32'h00_00_00_00),
-   //             .rights("RW")
-   //          );
-
+   
+   default_map.add_reg(
+      .rg    (cfg_reg_low),
+      .offset(32'h000),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (cfg_reg_hi),
+      .offset(32'h004),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_val_lo),
+      .offset(32'h008),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_val_hi),
+      .offset(32'h00c),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_cmp_lo),
+      .offset(32'h010),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_cmp_hi),
+      .offset(32'h014),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_start_lo),
+      .offset(32'h018),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_start_hi),
+      .offset(32'h01c),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_reset_lo),
+      .offset(32'h020),
+      .rights("RW")
+   );
+   default_map.add_reg(
+      .rg    (timer_reset_hi),
+      .offset(32'h024),
+      .rights("RW")
+   );
+   
 endfunction : add_regs_to_map
 
 
---
+++ dv/uvme_apb_timer/src/reg/uvme_apb_timer_timer_start_lo_reg.sv
@@ -0,0 +1,62 @@
+// 
+// Copyright 2021 Low Power Futures Inc.
+// 
+
+
+// WARNING: THE FOLLOWING IS AUTO-GENERATED CODE.
+// ANY CHANGES MADE HERE WILL BE ERASED UPON THE NEXT RELEASE OF THIS RAL.
+
+
+`ifndef __UVME_APB_TIMER_TIMER_START_LO_REG_SV__
+`define __UVME_APB_TIMER_TIMER_START_LO_REG_SV__
+
+
+class uvme_apb_timer_timer_start_lo_reg_c extends uvml_ral_reg_c;
+   
+   rand uvml_ral_reg_field_c  timer_start_lo; ///< Write strobe address for starting low counter
+   
+   
+   `uvm_object_utils_begin(uvme_apb_timer_timer_start_lo_reg_c)
+      `uvm_field_object(timer_start_lo, UVM_DEFAULT)
+   `uvm_object_utils_end
+   
+   
+   /**
+    * Default constructor.
+    */
+   extern function new(string name="uvme_apb_timer_timer_start_lo_reg", int unsigned n_bits=32, int has_coverage=UVM_NO_COVERAGE);
+   
+   /**
+    * Creates and configures register fields.
+    */
+   extern virtual function void build();
+   
+endclass : uvme_apb_timer_timer_start_lo_reg_c
+
+
+function uvme_apb_timer_timer_start_lo_reg_c::new(string name="uvme_apb_timer_timer_start_lo_reg", int unsigned n_bits=32, int has_coverage=UVM_NO_COVERAGE);
+   
+   super.new(name, n_bits, has_coverage);
+   
+endfunction : new
+
+
+function void uvme_apb_timer_timer_start_lo_reg_c::build();
+   
+   timer_start_lo = uvml_ral_reg_field_c::type_id::create("timer_start_lo");
+   timer_start_lo.configure(
+      .parent                 (this),
+      .size                   (   31),
+      .lsb_pos                (   0),
+      .access                 ("WO"),
+      .volatile               (   0),
+      .reset                  (   0),
+      .has_reset              (   1),
+      .is_rand                (   1),
+      .individually_accessible(   0)
+   );
+   
+endfunction: build
+
+
+`endif // __UVME_APB_TIMER_TIMER_START_LO_REG_SV__
