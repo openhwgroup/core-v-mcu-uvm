e0b093fb63fbfe00b6d4f11b91f377a5b8c19de4
David Poulin <55469439+datum-dpoulin@users.noreply.github.com>
2023-02-03 14:06:14 -0500

    Update instantiation.sv

--- dv/uvme_cvmcu/examples/instantiation.sv
+++ dv/uvme_cvmcu/examples/instantiation.sv
@@ -1,8 +1,7 @@
 // Copyright 2022-2023 Datum Technology Corporation
 // All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// This file contains sample code that demonstrates how to add an instance of the CORE-V MCU
-// Sub-System UVM Environment to another UVM environment.
+// This file contains sample code that demonstrates how to add an instance of the CORE-V MCU UVM Environment to another UVM environment.
 // NOTE: It is recommended to split up classes into separate files.
 
 
