ef8e4134821483c827303519d14aed8b2715856a
David Poulin <55469439+datum-dpoulin@users.noreply.github.com>
2023-02-03 14:17:05 -0500

    Update instantiation.sv

--- dv/uvme_cvmcu/examples/instantiation.sv
+++ dv/uvme_cvmcu/examples/instantiation.sv
@@ -5,18 +5,18 @@
 // NOTE: It is recommended to split up classes into separate files.
 
 
-`ifndef __MY_ENV_CFG_SV__
-`define __MY_ENV_CFG_SV__
+`ifndef __UVME_EXAMPLE_ENV_CFG_SV__
+`define __UVME_EXAMPLE_ENV_CFG_SV__
 
 
 /**
- * Object encapsulating all configuration information for my_env.
+ * Object encapsulating all configuration information for uvme_example_env_c.
  */
-class my_env_cfg_c extends uvm_object;
+class uvme_example_env_cfg_c extends uvmx_env_cfg_c;
 
    rand uvme_cvmcu_cfg_c  cvmcu_cfg; ///< Handle to cvmcu environment configuration
 
-   `uvm_object_utils_begin(my_env_cfg_c)
+   `uvm_object_utils_begin(uvme_example_env_cfg_c)
       `uvm_field_object(cvmcu_cfg, UVM_DEFAULT)
    `uvm_object_utils_end
 
@@ -28,102 +28,102 @@ class my_env_cfg_c extends uvm_object;
    }
 
    /**
+    * Default constructor.
+    */
+   function new(uvm_component parent=null, string name="uvme_example_env_cfg");
+      super.new(parent, name);
+   endfunction
+
+   /**
     * Creates sub-configuration objects
     */
-   function new(uvm_component parent=null, string name="my_env");
-      super.new(parent, name);
-      cfg = uvme_cvmcu_cfg_c::type_id::create("cvmcu_cfg");
-   endfunction : new
+   virtual function void create_objects();
+      cvmcu_cfg = uvme_cvmcu_cfg_c::type_id::create("cvmcu_cfg");
+   endfunction
 
-endclass : my_env_cfg_c
-
-
-`endif // __MY_ENV_CFG_SV__
+endclass : uvme_example_env_cfg_c
 
 
-`ifndef __MY_ENV_CNTXT_SV__
-`define __MY_ENV_CNTXT_SV__
+`endif // __UVME_EXAMPLE_ENV_CFG_SV__
+
+
+`ifndef __UVME_EXAMPLE_ENV_CNTXT_SV__
+`define __UVME_EXAMPLE_ENV_CNTXT_SV__
 
 
 /**
- * Object encapsulating all state variables for my_env.
+ * Object encapsulating all state variables for uvme_example_env_c.
  */
-class my_env_cntxt_c extends uvm_object;
+class uvme_example_env_cntxt_c extends uvmx_env_cntxt_c;
 
    uvme_cvmcu_cntxt_c  cvmcu_cntxt; ///< Handle to cvmcu environment context
 
-   `uvm_object_utils_begin(my_env_cntxt_c)
+   `uvm_object_utils_begin(uvme_example_env_cntxt_c)
       `uvm_field_object(cvmcu_cntxt, UVM_DEFAULT)
    `uvm_object_utils_end
 
    /**
     * Creates sub-context objects
     */
-   function new(uvm_component parent=null, string name="my_env");
+   function new(uvm_component parent=null, string name="uvme_example_env_cntxt");
       super.new(parent, name);
       cntxt = uvme_cvmcu_cntxt_c::type_id::create("cvmcu_cntxt");
-   endfunction : new
+   endfunction
 
-endclass : my_env_cntxt_c
+   /**
+    * Creates sub-context objects
+    */
+   virtual function void create_objects();
+      cvmcu_cntxt = uvme_cvmcu_cntxt_c::type_id::create("cvmcu_cntxt");
+   endfunction
+
+endclass : uvme_example_env_cntxt_c
 
 
-`endif // __MY_ENV_CNTXT_SV__
+`endif // __UVME_EXAMPLE_ENV_CNTXT_SV__
 
 
-`ifndef __MY_ENV_SV__
-`define __MY_ENV_SV__
+`ifndef __UVME_EXAMPLE_ENV_SV__
+`define __UVME_EXAMPLE_ENV_SV__
 
 
 /**
  * Component encapsulating the environment.
  */
-class my_env_c extends uvm_env;
+class uvme_example_env_c extends uvmx_env_c #(
+   .T_CFG      (uvme_example_env_cfg_c      ),
+   .T_CNTXT    (uvme_example_env_cntxt_c    ),
+   .T_VSQR     (uvme_example_env_vsqr_c     ),
+   .T_PRD      (uvme_example_env_prd_c      ),
+   .T_SB       (uvme_example_env_sb_c       ),
+   .T_COV_MODEL(uvme_example_env_cov_model_c)
+);
 
-   my_env_cfg_c    cfg  ; ///< Configuration handle.  Must be provided by component instantiating this environment.
-   my_env_cntxt_c  cntxt; ///< Context handle.  Can be provided by component instantiating this environment.
    uvme_cvmcu_env_c  cvmcu_env; ///< CORE-V MCU environment instance.
 
-   `uvm_component_utils_begin(my_env_c)
-      `uvm_field_object(cfg  , UVM_DEFAULT)
-      `uvm_field_object(cntxt, UVM_DEFAULT)
+   `uvm_component_utils_begin(uvme_example_env_c)
    `uvm_component_utils_end
 
    /**
     * Default constructor.
     */
-   function new(uvm_component parent=null, string name="my_env");
+   function new(uvm_component parent=null, string name="uvme_example_env");
       super.new(parent, name);
-   endfunction : new
+   endfunction
+   
+   virtual function void assign_cfg();
+      uvm_config_db#(uvme_cvmcu_cfg_c)::set(this, "cvmcu_env", "cfg", cfg.cvmcu_cfg);
+   endfunction
 
-   /**
-    * 1. Ensures #cfg & #cntxt handles are not null
-    * 2. Assigns #cfg and #cntxt handles
-    * 3. Creates #cvmcu_env
-    */
-   virtual function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
+   virtual function void assign_cntxt();
+      uvm_config_db#(uvme_cvmcu_cntxt_c)::set(this, "cvmcu_env", "cntxt", cfg.cvmcu_cntxt);
+   endfunction
 
-      void'(uvm_config_db#(my_env_cfg_c)::get(this, "", "cfg", cfg));
-      if (!cfg) begin
-         `uvm_fatal("MY_ENV", "Configuration handle is null")
-      end
-      else begin
-         `uvm_info("MY_ENV", $sformatf("Found configuration handle:\n%s", cfg.sprint()), UVM_DEBUG)
-         void'(uvm_config_db#(my_env_cntxt_c)::get(this, "", "cntxt", cntxt));
-         if (!cntxt) begin
-            `uvm_info("MY_ENV", "Context handle is null; creating.", UVM_DEBUG)
-            cntxt = my_env_cntxt_c::type_id::create("cntxt");
-         end
-         uvm_config_db#(my_env_cfg_c  )::set(this, "*", "cfg"  , cfg  );
-         uvm_config_db#(my_env_cntxt_c)::set(this, "*", "cntxt", cntxt);
-         uvm_config_db#(uvme_cvmcu_cfg_c  )::set(this, "cvmcu_env", "cfg"  , cfg  .cvmcu_cfg  );
-         uvm_config_db#(uvme_cvmcu_cntxt_c)::set(this, "cvmcu_env", "cntxt", cntxt.cvmcu_cntxt);
-         cvmcu_env = uvme_cvmcu_env_c::type_id::create("cvmcu_env", this);
-         end
-      end
-   endfunction : build_phase
+   virtual function void create_envs();
+      cvmcu_env = uvme_cvmcu_env_c::type_id::create("cvmcu_env", this);
+   endfunction
 
-endclass : my_env_c
+endclass : uvme_example_env_c
 
 
-`endif // __MY_ENV_SV__
+`endif // __UVME_EXAMPLE_ENV_SV__
