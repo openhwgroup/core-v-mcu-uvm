6b63018c0ea65424039a3366fd936e7083f83ca2
datum-dpoulin <dpoulin@datumtc.ca>
2023-12-06 14:49:25 -0500

    Updating UART eg seq

--- dv/chip/uvme_cvmcu_chip/src/seq/uvme_cvmcu_chip_uart_eg_seq.sv
+++ dv/chip/uvme_cvmcu_chip/src/seq/uvme_cvmcu_chip_uart_eg_seq.sv
@@ -62,10 +62,24 @@ class uvme_cvmcu_chip_uart_eg_seq_c extends uvme_cvmcu_chip_base_seq_c;
     * TODO Describe uvme_cvmcu_chip_uart_eg_seq_c::body()
     */
    virtual task body();
+      setup_dut();
       fork
          uart0();
          uart1();
       join
+   endtask
+
+   /**
+    * Drive registers to setup UART0 and UART1
+    */
+   task setup_dut();
+      // Configuration fields for the UART agents
+      // rand int unsigned  baud_rate; ///< BAUD rate
+      // rand int unsigned  frame_size; ///< Number of data bits in a frame
+      // rand int unsigned  num_stop_bits; ///< Number of STOP bits in a frame
+      // rand bit  enable_parity_bit; ///< Enables adding a parity bit at the end of frame.
+      // rand uvma_uart_parity_enum  parity_type; ///< When parity is enabled, specifies ODD/EVEN parity.
+      // reg_model.ABC.DEF.write();
    endtask
 
    /**
@@ -75,26 +89,30 @@ class uvme_cvmcu_chip_uart_eg_seq_c extends uvme_cvmcu_chip_base_seq_c;
       uvma_uart_seq_item_c  item;
       uvma_uart_mon_trn_c  trn;
       int unsigned  gap_size;
+      bit  data[$];
       for (int unsigned ii=0; ii<num_items; ii++) begin
          gap_size = $urandom_range(min_gap, max_gap);
-         `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Waiting %0d gap cycle(s) before item %0d/%0d", gap_size, (ii+1), num_items), UVM_HIGH)
-         clk(gap_size);
-         `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Starting item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_MEDIUM)
+         //`uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Waiting %0d gap cycle(s) before item %0d/%0d", gap_size, (ii+1), num_items), UVM_HIGH)
+         //clk(gap_size);
+         //`uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Starting item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_MEDIUM)
          item = uvma_uart_seq_item_c::type_id::create("item");
          item.cfg = cfg.uart0_agent_cfg;
          item.cntxt = cfg.uart0_agent_cntxt;
          if !(item.randomize()) begin
             `uvm_fatal("CVMCU_CHIP_UART_EG_SEQ", "Failed to randomize UART item")
          end
-         // TODO Drive the register model to output this item
-         //      item.data
-         //      item.parity
-         //      cfg.uart0_agent_cfg.baud_rate
+         foreach (item.data[ii]) begin
+            data.push_back(item.data[ii]);
+         end
+         if (cfg.uart0_agent_cfg.enable_parity_bit) begin
+            data.push_back(item.parity);
+         end
          trn = uvma_uart_mon_trn_c::type_id::create("trn");
          trn.copy(item);
          udma_uart0_egress_exp_ap.write(trn);
          `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Finished item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_HIGH)
       end
+      // TODO Drive the register model to output data[$]
    endtask
 
    /**
@@ -104,25 +122,30 @@ class uvme_cvmcu_chip_uart_eg_seq_c extends uvme_cvmcu_chip_base_seq_c;
       uvma_uart_seq_item_c  item;
       uvma_uart_mon_trn_c  trn;
       int unsigned  gap_size;
+      bit  data[$];
       for (int unsigned ii=0; ii<num_items; ii++) begin
          gap_size = $urandom_range(min_gap, max_gap);
-         `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Waiting %0d gap cycle(s) before item %0d/%0d", gap_size, (ii+1), num_items), UVM_HIGH)
-         clk(gap_size);
-         `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Starting item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_MEDIUM)
+         //`uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Waiting %0d gap cycle(s) before item %0d/%0d", gap_size, (ii+1), num_items), UVM_HIGH)
+         //clk(gap_size);
+         //`uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Starting item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_MEDIUM)
          item = uvma_uart_seq_item_c::type_id::create("item");
          item.cfg = cfg.uart1_agent_cfg;
          item.cntxt = cfg.uart1_agent_cntxt;
          if !(item.randomize()) begin
             `uvm_fatal("CVMCU_CHIP_UART_EG_SEQ", "Failed to randomize UART item")
          end
-         // TODO Drive the register model to output this item
-         //      item.data
-         //      item.parity
-         //      cfg.uart1_agent_cfg.baud_rate
+         foreach (item.data[ii]) begin
+            data.push_back(item.data[ii]);
+         end
+         if (cfg.uart1_agent_cfg.enable_parity_bit) begin
+            data.push_back(item.parity);
+         end
          trn = uvma_uart_mon_trn_c::type_id::create("trn");
          trn.copy(item);
          udma_uart1_egress_exp_ap.write(trn);
          `uvm_info("CVMCU_CHIP_UART_EG_SEQ", $sformatf("Finished item #%0d of %0d with gap size %0d", (ii+1), num_items, gap_size), UVM_HIGH)
+      end
+      // TODO Drive the register model to output data[$]
       end
    endtask
 
