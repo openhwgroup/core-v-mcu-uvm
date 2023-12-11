038f4c0dbdb57032b3cb68e74fc1a3e62ea126e3
datum-dpoulin <dpoulin@datumtc.ca>
2023-12-11 15:22:59 -0500

    Adding uart knobs to test_cfg

--- dv/chip/uvmt_cvmcu_chip/src/uvmt_cvmcu_chip_test_cfg.sv
+++ dv/chip/uvmt_cvmcu_chip/src/uvmt_cvmcu_chip_test_cfg.sv
@@ -17,14 +17,20 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
 
    /// @name Knobs
    /// @{
+   rand int unsigned  uart0_baud_rate; ///< Baud rate for UART0.
+   rand int unsigned  uart1_baud_rate; ///< Baud rate for UART1.
    rand int unsigned  sys_clk_frequency; ///< System frequency (Hz).
    rand int unsigned  jtag_clk_frequency; ///< JTAG frequency (Hz).
+   rand int unsigned  uart0_clk_frequency; ///< UART0 agent clock frequency (Hz).
+   rand int unsigned  uart1_clk_frequency; ///< UART1 agent clock frequency (Hz).
    /// @}
 
    /// @name Agents
    /// @{
    rand uvma_clk_cfg_c    sys_clk_agent_cfg; ///< System clock agent configuration.
    rand uvma_clk_cfg_c    jtag_clk_agent_cfg; ///< JTAG clock agent configuration.
+   rand uvma_clk_cfg_c    uart0_clk_agent_cfg; ///< UART0 agent clock clock agent configuration.
+   rand uvma_clk_cfg_c    uart1_clk_agent_cfg; ///< UART1 agent clock clock agent configuration.
    rand uvma_reset_cfg_c  sys_reset_agent_cfg; ///< System reset agent configuration.
    rand uvma_reset_cfg_c  jtag_reset_agent_cfg; ///< JTAG reset agent configuration.
    /// @}
@@ -33,6 +39,8 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
    `uvm_object_utils_begin(uvmt_cvmcu_chip_test_cfg_c)
       `uvm_field_int(sys_clk_frequency, UVM_DEFAULT + UVM_DEC)
       `uvm_field_int(jtag_clk_frequency, UVM_DEFAULT + UVM_DEC)
+      `uvm_field_int(uart0_clk_frequency, UVM_DEFAULT + UVM_DEC)
+      `uvm_field_int(uart1_clk_frequency, UVM_DEFAULT + UVM_DEC)
       `uvm_field_int(cli_block_name_override, UVM_DEFAULT)
       `uvm_field_string(cli_block_name, UVM_DEFAULT)
       `uvm_field_object(selected_reg_block, UVM_DEFAULT)
@@ -45,11 +53,20 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
       `uvm_field_int(simulation_timeout, UVM_DEFAULT + UVM_DEC)
       `uvm_field_object(sys_clk_agent_cfg, UVM_DEFAULT)
       `uvm_field_object(jtag_clk_agent_cfg, UVM_DEFAULT)
+      `uvm_field_object(uart0_clk_agent_cfg, UVM_DEFAULT)
+      `uvm_field_object(uart1_clk_agent_cfg, UVM_DEFAULT)
       `uvm_field_object(sys_reset_agent_cfg, UVM_DEFAULT)
       `uvm_field_object(jtag_reset_agent_cfg, UVM_DEFAULT)
    `uvm_object_utils_end
 
 
+   /**
+    * Describes randomization space for knobs.
+    */
+   constraint knobs_cons {
+      uart0_baud_rate inside {'d4800,'d9600,'d19200,'d57600,'d115200};
+      uart1_baud_rate inside {'d4800,'d9600,'d19200,'d57600,'d115200};
+   }
 
    /**
     * Sets safe defaults parameters.
@@ -73,6 +90,10 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
       sys_clk_agent_cfg.is_active == UVM_ACTIVE;
       jtag_clk_agent_cfg.enabled == 1;
       jtag_clk_agent_cfg.is_active == UVM_ACTIVE;
+      uart0_clk_agent_cfg.enabled == 1;
+      uart0_clk_agent_cfg.is_active == UVM_ACTIVE;
+      uart1_clk_agent_cfg.enabled == 1;
+      uart1_clk_agent_cfg.is_active == UVM_ACTIVE;
       sys_reset_agent_cfg.enabled == 1;
       sys_reset_agent_cfg.is_active == UVM_ACTIVE;
       sys_reset_agent_cfg.polarity == UVMX_RESET_ACTIVE_LOW;
@@ -84,9 +105,11 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
    }
 
    /**
-    * TODO Implement or remove uvmt_cvmcu_chip_test_cfg_c::rules_cons
+    * Constrains UART agent clock frequencies to baud rates.
     */
    constraint rules_cons {
+      uart0_clk_frequency == uart0_baud_rate;
+      uart1_clk_frequency == uart1_baud_rate;
    }
 
 
@@ -103,6 +126,8 @@ class uvmt_cvmcu_chip_test_cfg_c extends uvmx_chip_test_cfg_c #(
    virtual function void build();
       sys_clk_agent_cfg = uvma_clk_cfg_c::type_id::create("sys_clk_agent_cfg");
       jtag_clk_agent_cfg = uvma_clk_cfg_c::type_id::create("jtag_clk_agent_cfg");
+      uart0_clk_agent_cfg = uvma_clk_cfg_c::type_id::create("uart0_clk_agent_cfg");
+      uart1_clk_agent_cfg = uvma_clk_cfg_c::type_id::create("uart1_clk_agent_cfg");
       sys_reset_agent_cfg = uvma_reset_cfg_c::type_id::create("sys_reset_agent_cfg");
       jtag_reset_agent_cfg = uvma_reset_cfg_c::type_id::create("jtag_reset_agent_cfg");
    endfunction
